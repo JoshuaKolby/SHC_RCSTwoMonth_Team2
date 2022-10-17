975f6ef2e8d7d6e2e02210ee698bc1a1d46ceed4
JoshuaKolby <115902092+JoshuaKolby@users.noreply.github.com>
2022-10-17 16:31:06 -0500

    Add files via upload

--- flightcode.001.ino
+++ flightcode.001.ino
@@ -1,17 +1,19 @@
 /*RCS Team 2 Program
  * IF YOU ARE TESTING BE SURE TO COMMENT OUT PARTS OF CODE YOU ARE NOT USING
- * IF USING THE SERIAL MONITOR TO READ DATA, CHANGE ALL SDCARD.PRINT TO SERIAL.PRINT OTHERWISE YOU WILL NOT SEE ANYTHING
+ * IF USING THE SERIAL MONITOR TO READ DATA, CHANGE ALL Serial.PRINT TO SERIAL.PRINT OTHERWISE YOU WILL NOT SEE ANYTHING
  */
 
 #include <SoftwareSerial.h>
 #include <Arduino_LSM6DSOX.h>
-#include <BMP388_DEV.h> 
 #include <Wire.h>
 #include <SparkFun_u-blox_GNSS_Arduino_Library.h>
+#include <BMP388_DEV.h>                           // Include the BMP388_DEV.h library
 
-float temperature, pressure, altitude, flighttime, startingaltitude, maxalt; //sensor values, do not need to be assigned
+float temperature, pressure, altitude;            // Create the temperature, pressure and altitude variables
 BMP388_DEV bmp388;
-SoftwareSerial SDCard (0, 1):
+
+float flighttime, startingaltitude, maxalt; //sensor values, do not need to be assigned
+SoftwareSerial SDCard(0, 1);
 SFE_UBLOX_GNSS myGNSS;
 
 int const ledPin = 3;                                            //New Energy Led Pin
@@ -20,7 +22,7 @@ int const SolenoidCounterClockwise = 5;                          //pin for Solen
 int const camera = 6;                                            //pin for camera
 float altForStabilization = 25000;                               //altitude that satbilization will start
 float gyroscopestop = 0.5f;                                      //gyroscope limiter to detect when it has stopped moving
-float minDegSec = 30;                                            //target degrees/second of spin, enable solenoids when passed in the positive or negative direction
+float maxDegSec = 30;                                            //target degrees/second of spin, enable solenoids when passed in the positive or negative direction
 int ledcounter = 4;                                              //the amount of time between blinks (ledcounter/4) = the amount of blinks per second
 int pictureOnTime = 50;                                          //the delay between signals for picture taking
 int videoOnTime = 200;                                           //the delay between signals for starting and stopping video
@@ -33,14 +35,17 @@ bool ascending;                                                  //bool signalin
 bool pictureOn;                                                  //bool to take pictures
 bool videoOnUpdate;                                              //bool to take videos
 bool videoOnMain;                                                //determines if we are starting a new video or stopping a old one
-long lastTime = 0; //for ZOE                                     //long for ZOE (I don't actually know how this works, it was just in the example)
+long lastTime = 0;                                               //long for ZOE (I don't actually know how this works, it was just in the example)
 
-void setup() 
-{
+void setup() {
+  
   Serial.begin(115200);                                          //baud rate is 115200, if something doesn't work, check baud first
-  SDCard.begin(115200);                                          //IF TESTING ON COMPUTER CHANGE ALL SDCARD.PRINT TO SERIAL.PRINT AND COMMENT OUT PARTS YOU DON'T NEED AT THE MOMENT
-  Wire.begin();
-
+  SDCard.begin(115200);                                          //IF TESTING ON COMPUTER CHANGE ALL Serial.PRINT TO SERIAL.PRINT AND COMMENT OUT PARTS YOU DON'T NEED AT THE MOMENT
+  Wire.begin(); 
+  bmp388.begin();                                 // Default initialisation, place the BMP388 into SLEEP_MODE 
+  bmp388.setTimeStandby(TIME_STANDBY_1280MS);     // Set the standby time to 1.3 seconds
+  bmp388.startNormalConversion();   
+  
   pinMode(SolenoidClockwise, OUTPUT);
   pinMode(SolenoidCounterClockwise, OUTPUT);
   pinMode(camera, OUTPUT);
@@ -51,101 +56,114 @@ void setup()
   flighttime = 0; packetcount = 0; launchstate = 0;
   
   IMU.begin();                                                  //begin sensors
-  bmp388.begin();
-  bmp388.setTimeStandby(TIME_STANDBY_1280MS);
-  bmp388.startNormalConversion();
-  startingaltitude = bmp388.getMeasurements(altitude);
-  if (myGNSS.begin() == false) //Connect to the u-blox module using Wire port {
-    SDCard.println(F("GPS did not work you idiot. Freezing."));
+  //startingaltitude = bmp388.getMeasurements(altitude);
+  if (myGNSS.begin() == false) { //Connect to the u-blox module using Wire port
+    Serial.println(F("GPS did not work you idiot. Freezing."));
+    digitalWrite(ledPin, HIGH);
     while (1);
   }
-  while(!IMU.begin() || !bmp388.begin()){
-    SDcard.println("sensor initiation failed")
+  while(!IMU.begin()){
+    Serial.println("sensor initiation failed");
     digitalWrite(ledPin, HIGH);
+    while (1);
   }  
   
   myGNSS.setI2COutput(COM_TYPE_UBX);
   myGNSS.saveConfigSelective(VAL_CFG_SUBSEC_IOPORT);
   
-  SDCard.println("TEAM_ID, MISSION_TIME, PACKET_COUNT, SW_STATE, CAM_STATE, REAL_TIME, LAT, LONG, SIV, PRESSURE, ALTITUDE, TEMP, ACC_X, ACC_Y, ACC_Z, GYRO_X, GYRO_Y, GYRO_Z");
+  Serial.println("TEAM_ID, MISSION_TIME, PACKET_COUNT, SW_STATE, CAM_STATE, REAL_TIME, LAT, LONG, SIV, PRESSURE, ALTITUDE, TEMP, ACC_X, ACC_Y, ACC_Z, GYRO_X, GYRO_Y, GYRO_Z");
 }
 
 void loop() {                                                   //loop function, make sure last print statement is println, but no other print statements are
 
 //recording and logging data -------------------------------------------------------------------------------------------------------------------------------------------------- NEEDS TESTING
 
-  SDCard.print("RCS Team 2 (HMMRHD), ");
-  SDcard.print(flighttime); SDcard.print(", ");                 //record time into flight
-  SDCard.print(packetcount); SDcard.print(", ");
-  SDCard.print(launchstate); SDcard.print(", ");
-  SDCard.print("Camera state"); SDcard.print(", ");
+  Serial.print("RCS Team 2 (HMMRHD), ");           //can combine if we want
+  Serial.print(flighttime + ", ");                 //record time into flight
+  Serial.print(packetcount + ", ");
+  Serial.print(launchstate + ", ");
+  Serial.print("Camera state" + ", ");
 
   if (millis() - lastTime > 1000)                               //GPS Data (Lat, Long, SIV)
   {
     lastTime = millis();
 
-    SDcard.print(myGNSS.getHour()); SDcard.print(":");
-    SDcard.print(myGNSS.getMinute()); SDcard.print(":");
-    SDcard.print(myGNSS.getSecond()); SDcard.print(", ");
+    Serial.print(myGNSS.getHour() + ":");
+    Serial.print(myGNSS.getMinute() + ":");
+    Serial.print(myGNSS.getSecond() ", ");
     
     long latitude = myGNSS.getLatitude();
-    SDCard.print(latitude); SDcard.print(", "); 
+    Serial.print(latitude + ", "); 
 
     long longitude = myGNSS.getLongitude();
-    SDCard.print(longitude); SDcard.print(", "); 
+    Serial.print(longitude + ", ");
 
     byte SIV = myGNSS.getSIV();
-    SDCard.println(SIV); SDcard.println(", "); 
-  } else { SDCard.println("-, -, -, "); }
-  
-  if (bmp388.getMeasurements(temperature, pressure, altitude))  //bmp388 data
-  { 
-    SDCard.print(pressure);  SDcard.print(", ");    
-    SDCard.print(altitude);  SDcard.print(", ");
-    SDCard.print(temperature); SDcard.print(", ");   
-  } else { SDCard.print("-, -, -, ");
+    Serial.print(SIV + ", ");
+  } else { Serial.print("-, -, -, "); }
 
+  if (bmp388.getMeasurements(temperature, pressure, altitude))    // Check if the measurement is complete
+  {
+    Serial.print(temperature + ", ");
+    Serial.print(pressure + ", ");    
+    Serial.print(altitude + ", ");
+  } else { Serial.print("-, -, -, "); }
+  
   float ax, ay, az;
   if (IMU.accelerationAvailable()) {                            //IMU data acceleration
     IMU.readAcceleration(ax, ay, az);
 
-    SDCard.print(ax); SDcard.print(", ");
-    SDCard.print(ay); SDcard.print(", ");
-    SDCard.print(az); SDcard.print(", ");
-  }
+    Serial.print(ax + ", ");
+    Serial.print(ay + ", ");
+    Serial.print(az + ", ");
+  } else { Serial.print("-, -, -, "); }
   
   float gx, gy, gz;
   if (IMU.gyroscopeAvailable()) {                               //IMU data gyro(in deg/sec)
     
     IMU.readGyroscope(gx, gy, gz);
 
-    SDCard.print(gx); SDcard.print(", "); 
-    SDCard.print(gy); SDcard.print(", "); 
-    SDCard.println(gz); SDcard.print(", "); 
-  } else { SDCard.print("-, -, -, ");
+    Serial.print(gx + ", "); 
+    Serial.print(gy + ", "); 
+    Serial.print(gz + ", ");
+  } else { Serial.print("-, -, -, "); }
   
   if(altitude > maxalt){ maxalt = altitude; }
 
 //determine state of vehicle ------------------------------------------------------------------------------------------------------------------------------------------------ NEEDS TESTING
 
-  if(launchstate == 0 && (altitude+25) > startingalt) { launchstate = 1; }                                      //ascending
-  if(launchstate == 1 && altitude > altforstabilization) { launchstate = 2; }                                   //control active
-  if(launchstate == 2 && !stabilization) {launchstate = 3; }                                                    //control deactive
-  if(launchstate == 3 && stabilization) { launchstate = 2; }                                                    //control deactive
-  if(launchstate == 2 && maxalt < (altitude+25)) { launchstate = 4; }                                           //desending
-  if(launchstate == 3 && maxalt < (altitude+25)) { launchstate = 4; }                                           //desending
-  if(launchstate == 4 && gy <= gyroscopestop && gx <= gyroscopestop && gz <= gyroscopestop) {launchstate = 5;}  //landed
+  if(launchstate == 0 && (altitude+25) > startingalt) { 
+    launchstate = 1;                                                                                            //ascending
+  }
+  if(launchstate == 1 && altitude > altforstabilization) { 
+    launchstate = 2;                                                                                            //control active
+  }
+  if(launchstate == 2 && !stabilization) { 
+    launchstate = 3;                                                                                            //control deactive
+  }
+  if(launchstate == 3 && stabilization) { 
+    launchstate = 2;                                                                                            //control deactive
+  }
+  if(launchstate == 2 && maxalt < (altitude+25)) { 
+    launchstate = 4;                                                                                            //desending
+  }
+  if(launchstate == 3 && maxalt < (altitude+25)) { 
+    launchstate = 4;                                                                                            //desending
+  }
+  if(launchstate == 4 && gy <= gyroscopestop && gx <= gyroscopestop && gz <= gyroscopestop) {
+    launchstate = 5;                                                                                            //landed
+  }
 
-//stabilization -------------------------------------------------------------------------------------------------------------------------------------------------------------  NEEDS TESTING
+//stabilization -------------------------------------------------------------------------------------------------------------------------------------------------------------  NEEDS TESTING *change to ||
 
-  if(altitude >= altForStabilization) {stabilizealt = true);                 //turn on at altitude
+  if(altitude >= altForStabilization) {stabilizealt = true);                               //turn on at altitude
   else { stabilizealt = false; }
   
-  if(gy >= minDegSec && stabilizealt) {                               //determine if we need to turn on Clockwise Solenoid
+  if(gy >= maxDegSec && stabilizealt) {                                                    //determine if we need to turn on Clockwise Solenoid
     digitalWrite(SolenoidClockwise, HIGH);
   } else { digitalWrite(SolenoidClockwise, LOW); }
  
-  if(gy <= -minDegSec && stabilizealt) {                               //determine if we need to turn on Counter Clockwise Solenoid
+  if(gy <= -maxDegSec && stabilizealt) {                                                   //determine if we need to turn on Counter Clockwise Solenoid
     digitalWrite(SolenoidCounterClockwise, HIGH);
   } else { digitalWrite(SolenoidCounterClockwise, LOW); }
 
@@ -218,6 +236,6 @@ void loop() {                                                   //loop function,
   }
 
   packetcount += 1;
-  flighttime += 0.25;                                         //add 0.25 sec to flight time
+  flighttime += 0.25;                                         //update to millis
   delay(250);                                                 //delay 0.25 sec (maybe change this to be a milli delay like the picture
 }
